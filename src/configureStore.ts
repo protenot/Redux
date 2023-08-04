2a3733d58dac653f5a23da17f87fb82d68ce7d43
Olga_Belaya <protenot@yandex.ru>
2023-08-04 08:57:07 +0300

    final commit

--- src/configureStore.ts
+++ src/configureStore.ts
@@ -3,7 +3,7 @@ export type Store<State = any, Action = { type: string }> = {
   dispatch(action: Action): any;
   subscribe(cb: () => void): () => void;
   replaceReducer(newReducer: Reducer<State, Action>): void;
-  // middlewares(): Middleware<State, Action>;
+  //  middlewares():any;
 };
 
 export type Action = {
@@ -16,9 +16,9 @@ export type Reducer<State, Action> = (
   action: Action | undefined,
 ) => State;
 
-export type Middleware<State, Action> = (
-  store: Store<State, Action>,
-) => (next: (action: Action) => any) => (action: Action) => any;
+export type Middleware<State, Action> = () => (
+  next: (action: Action) => any,
+) => (action: Action) => any;
 
 export type ConfigureStore<State, Action> = (
   reducer: Reducer<State, Action>,
@@ -29,7 +29,7 @@ export type ConfigureStore<State, Action> = (
 export function configureStore<State>(
   reducer: Reducer<State, Action>,
   initialState?: State,
-  middlewares?: Middleware<State, Action>,
+  // middlewares?: Middleware<State, Action>,
 ): Store {
   let state = initialState as State;
   let cbs: Set<Function> = new Set();
@@ -54,22 +54,6 @@ export function configureStore<State>(
     replaceReducer(newReducer: Reducer<State, Action>): void {
       reducer = newReducer;
     },
-
-    /*  middlewares (state:State, action:Action){
-        middlewares = new Set(newMiddlewares)
-        for (let middleware of middlewares as Middleware<any, any>[]){    
-            middleware=
-            (store) => (next) => (action) => {
-             console.log("dispatching", action);
-         const result = next(action);
-         console.log("next state", store.getState());
-         
-         //return result;
-     
-            }
-             } 
-   
-    }*/
   };
 }
 export function applyMiddleware(
@@ -78,7 +62,7 @@ export function applyMiddleware(
 ) {
   let { dispatch } = store;
   middlewares.forEach((middleware) => {
-    dispatch = middleware(store)(dispatch);
+    dispatch = middleware()(dispatch);
   });
   return { ...store, dispatch };
 }
